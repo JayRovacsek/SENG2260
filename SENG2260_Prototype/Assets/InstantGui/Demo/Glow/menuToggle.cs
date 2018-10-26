573a480fa661b4637e9fb4c3043717578d582708
CarpeScriptum <44194726+CarpeScriptum@users.noreply.github.com>
2018-10-26 21:59:08 +1100

    Menu toggle with B

---
+++ SENG2260_Prototype/Assets/InstantGui/Demo/Glow/menuToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuToggle : MonoBehaviour {
+    private GameObject menu;
+
+    public menuToggle(GameObject menu)
+    {
+        
+    }
+	// Use this for initialization
+	void Start () {
+	menu = GameObject.FindWithTag("Menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.B))
+        {
+            menu.SetActive(!menu.activeSelf);
+        }
+	}
+}
