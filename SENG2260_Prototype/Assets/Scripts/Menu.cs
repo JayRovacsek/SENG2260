8215767b7bf88a6d3c3ee589b33bd96427ccecf0
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-27 11:16:50 +1100

    Hacky workarounds to disable player.

--- SENG2260_Prototype/Assets/Scripts/Menu.cs
+++ SENG2260_Prototype/Assets/Scripts/Menu.cs
@@ -5,18 +5,52 @@ using UnityEngine;
 public class Menu : MonoBehaviour {
 
     public GameObject PlayerMenu { get; set; }
+    public GameObject Player { get; set; }
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController FirstPersonController { get; set; }
+    public Camera Camera { get; set; }
+    public bool FreezeCamera { get; set; }
 
 	// Use this for initialization
 	void Start () {
         PlayerMenu = GameObject.FindWithTag("Menu");
+        Player = GameObject.FindWithTag("Player");
+        FirstPersonController = Player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        Camera = Player.GetComponent<Camera>();
         PlayerMenu.SetActive(false);
-	}
+        FreezeCamera = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetKeyDown(KeyCode.B))
         {
             PlayerMenu.SetActive(!PlayerMenu.activeSelf);
         }
-	}
+
+        if (PlayerMenu.activeSelf)
+        {
+            PlayerIsFrozen(true);
+        }
+        else
+        {
+            PlayerIsFrozen(false);
+        }
+    }
+
+    private void PlayerIsFrozen(bool status)
+    {
+        Player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = !status;
+
+        if (status)
+        {
+            FirstPersonController.m_WalkSpeed = 0;
+            FirstPersonController.m_RunSpeed = 0;
+        }
+        else
+        {
+            FirstPersonController.m_WalkSpeed = 5;
+            FirstPersonController.m_RunSpeed = 10;
+        }
+    }
 }
--- SENG2260_Prototype/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ SENG2260_Prototype/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -10,37 +10,37 @@ namespace UnityStandardAssets.Characters.FirstPerson
     [RequireComponent(typeof (AudioSource))]
     public class FirstPersonController : MonoBehaviour
     {
-        [SerializeField] private bool m_IsWalking;
-        [SerializeField] private float m_WalkSpeed;
-        [SerializeField] private float m_RunSpeed;
-        [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-        [SerializeField] private float m_JumpSpeed;
-        [SerializeField] private float m_StickToGroundForce;
-        [SerializeField] private float m_GravityMultiplier;
-        [SerializeField] private MouseLook m_MouseLook;
-        [SerializeField] private bool m_UseFovKick;
-        [SerializeField] private FOVKick m_FovKick = new FOVKick();
-        [SerializeField] private bool m_UseHeadBob;
-        [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
-        [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
-        [SerializeField] private float m_StepInterval;
-        [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+        [SerializeField] public bool m_IsWalking;
+        [SerializeField] public float m_WalkSpeed;
+        [SerializeField] public float m_RunSpeed;
+        [SerializeField] [Range(0f, 1f)] public float m_RunstepLenghten;
+        [SerializeField] public float m_JumpSpeed;
+        [SerializeField] public float m_StickToGroundForce;
+        [SerializeField] public float m_GravityMultiplier;
+        [SerializeField] public MouseLook m_MouseLook;
+        [SerializeField] public bool m_UseFovKick;
+        [SerializeField] public FOVKick m_FovKick = new FOVKick();
+        [SerializeField] public bool m_UseHeadBob;
+        [SerializeField] public CurveControlledBob m_HeadBob = new CurveControlledBob();
+        [SerializeField] public LerpControlledBob m_JumpBob = new LerpControlledBob();
+        [SerializeField] public float m_StepInterval;
+        [SerializeField] public AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+        [SerializeField] public AudioClip m_JumpSound;           // the sound played when character leaves the ground.
+        [SerializeField] public AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
-        private Camera m_Camera;
-        private bool m_Jump;
-        private float m_YRotation;
-        private Vector2 m_Input;
-        private Vector3 m_MoveDir = Vector3.zero;
-        private CharacterController m_CharacterController;
-        private CollisionFlags m_CollisionFlags;
-        private bool m_PreviouslyGrounded;
-        private Vector3 m_OriginalCameraPosition;
-        private float m_StepCycle;
-        private float m_NextStep;
-        private bool m_Jumping;
-        private AudioSource m_AudioSource;
+        public Camera m_Camera;
+        public bool m_Jump;
+        public float m_YRotation;
+        public Vector2 m_Input;
+        public Vector3 m_MoveDir = Vector3.zero;
+        public CharacterController m_CharacterController;
+        public CollisionFlags m_CollisionFlags;
+        public bool m_PreviouslyGrounded;
+        public Vector3 m_OriginalCameraPosition;
+        public float m_StepCycle;
+        public float m_NextStep;
+        public bool m_Jumping;
+        public AudioSource m_AudioSource;
 
         // Use this for initialization
         private void Start()
